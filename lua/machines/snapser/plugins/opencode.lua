e0a5390660414976ea3d494ebfa2e70e578929ee
Matt Monroe <matt@snapser.com>
2026-05-19 10:07:41 -0600

    Switch snapser comp to use claude instead of opencode :(

--- lua/machines/snapser/plugins/opencode.lua
+++ lua/machines/snapser/plugins/opencode.lua
@@ -1,6 +1,6 @@
 return {
     'NickvanDyke/opencode.nvim',
-    enabled = true,
+    enabled = false,
     dependencies = { 'folke/snacks.nvim', opts = { input = {}, picker = {}, terminal = {} } },
     config = function()
         vim.g.opencode_opts = {
